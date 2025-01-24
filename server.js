6219873430026b667a3566a2691172da48bb51bc
MaxChuloshnikov <89795931+Chuloshnikov@users.noreply.github.com>
2025-01-24 18:38:33 +0200

    +

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const dotenv = require('dotenv');
 
 
 const app = express();
@@ -12,10 +13,13 @@ const port = process.env.PORT || 8080;
 app.use(bodyParser.json());
 
 
+//________________________________________________________
+
+
 app.get('/', (req, res) => {
     res.send('Hello server!');
 });
 
 app.listen(port, () => {
-    console.log(`Server started on http://localhost:${port}`);
+    console.log(`Server started on port: ${port}`);
 });
