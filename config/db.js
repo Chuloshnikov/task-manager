8ea45ae4e1bd6df1170c7b67dcf5eaea96005748
MaxChuloshnikov <89795931+Chuloshnikov@users.noreply.github.com>
2025-01-24 19:16:31 +0200

    done

---
+++ config/db.js
@@ -0,0 +1,21 @@
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const connectDB = async () => {
+  try {
+    // Подключение к MongoDB
+    const conn = await mongoose.connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error(`Error: ${error.message}`);
+    process.exit(1); // Завершаем процесс при ошибке подключения
+  }
+};
+
+module.exports = connectDB;
