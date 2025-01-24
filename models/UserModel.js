4ed00d93f05e0991751b2f104176ac56ff8c3eda
MaxChuloshnikov <89795931+Chuloshnikov@users.noreply.github.com>
2025-01-24 19:50:17 +0200

    auth controller added

--- models/UserModel.js
+++ models/UserModel.js
@@ -1,28 +1,30 @@
 import { mongoose, model, models } from "mongoose";
 
-
-const UserSchema = mongoose.Schema({
+const UserSchema = mongoose.Schema(
+  {
     firstName: {
-        type: String,
-        require: true
+      type: String,
+      require: true,
     },
     lastName: {
-        type: String,
-        require: true
+      type: String,
+      require: true,
     },
     email: {
-        type: String,
-        require: true,
-        unique: true
+      type: String,
+      require: true,
+      unique: true,
     },
     password: {
-        type: String,
-        require: true
+      type: String,
+      require: true,
     },
     role: {
-        type: String,
-        default: 'user' //or admin
-    }
-}, {timestamps: true})
+      type: String,
+      default: "user", //or admin
+    },
+  },
+  { timestamps: true },
+);
 
-export const User = models?.User || model('User', UserSchema);
+export const User = models?.User || model("User", UserSchema);
