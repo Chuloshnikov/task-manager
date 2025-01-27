d50033f2530b66dbaee7e06cc28fbfc247250b87
MaxChuloshnikov <89795931+Chuloshnikov@users.noreply.github.com>
2025-01-27 14:12:00 +0200

    done

--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -17,7 +17,72 @@ const router = express.Router();
 router.use(checkAuth);
 
 
+//YAML!!!
+
+/** 
+* @openapi
+* '/api/task':
+* post:
+*    tags:
+*    - Task
+*    summary: Create a task
+*    security:
+*      - basicAuth: []
+*    requestBody:
+*     required: true
+*     content:
+*       application/json
+*         schema:
+*           type: array
+*           items:
+*            type: object
+*             properties:
+*              description:
+*               type: string
+*                default: Buy a book
+*     responses:
+*     201:
+*      description: Created
+*     400
+*       description: Bad Request
+*/
+
+
+
 router.post('/task', createTask);
+
+/** 
+* @openapi
+* '/api/task':
+* post:
+*    tags:
+*    - Task
+*    summary: Create a task
+*    security:
+*      - basicAuth: []
+*    responses:
+*     200:
+*      description: Success
+*     content:
+*       application/json
+*         schema:
+*           type: array
+*           items:
+*            type: object
+*             properties:
+*              id:
+*               type: string
+*              description:
+*               type: string
+*              completed:
+*               type: boolean
+*              createdBy:
+*               type: string
+*                default: Buy a book
+*     400
+*       description: Bad Request
+*/
+
 
 router.get('/task', getTasksByAuthor);
 router.get('/task/all', checkAdmin, getAllTasks);
--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 import express from "express";
 import bodyParser from "body-parser";
 import dotenv from "dotenv";
+import swaggerUI from 'swagger-ui-express';
 
 
 // Routes
 import authRouter from './routes/authRoutes.js'; // Путь к файлу с роутами auth
 import taskRouter from './routes/taskRoutes.js'; // Путь к файлу с роутами tasks
+import swaggerSpec from './config/swagger.js' //Путь к файлу документации
 
 const app = express();
 
@@ -16,9 +18,14 @@ const port = process.env.PORT || 8080;
 // Middleware
 app.use(bodyParser.json());
 
+
+//documentation route
+app.use('/api/docs', swaggerUI.serve, swaggerUI.setup(swaggerSpec));
 // Use Routes
 app.use('/api', authRouter);
 app.use('/api', taskRouter);
+
+
 
 
 app.listen(port, () => {
